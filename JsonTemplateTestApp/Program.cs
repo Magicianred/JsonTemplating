05e7552152d1e2c016b1e84b659819b65d6df37d
Tom A <tomallen35@hotmail.com>
2017-03-20 21:31:53 +0000

    Initial commit

---
+++ JsonTemplateTestApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using JsonTemplating;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonTemplateTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var template = string.Join("", File.ReadAllLines("template.txt"));
+            var test = new Test
+            {
+                Key = "keyValue",
+                Name = "nameValue",
+                Code = "codeValue",
+                Related = new Test
+                {
+                    Key = "childKeyValue",
+                    Name = "childNameValue",
+                    Code = "childCodeValue"
+                }
+            };
+
+            var result = JsonTemplateVisitor.Serialize(test, template);
+        }
+    }
+
+    class Test
+    {
+        public string Key { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public Test Related { get; set; }
+    }
+}
---
+++ JsonTemplating/JsonVisitor.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace JsonTemplating
+{
+    public abstract class JsonVisitor
+    {
+        public virtual JToken Visit(JToken token)
+        {
+            var clone = token.DeepClone();
+            return VisitInternal(clone);
+        }
+
+        protected virtual JToken VisitInternal(JToken token)
+        {
+            switch (token.Type)
+            {
+                case JTokenType.Object:
+                    return VisitObject((JObject)token);
+                case JTokenType.Property:
+                    return VisitProperty((JProperty)token);
+                case JTokenType.Array:
+                    return VisitArray((JArray)token);
+                case JTokenType.String:
+                case JTokenType.Integer:
+                case JTokenType.Float:
+                case JTokenType.Date:
+                case JTokenType.Boolean:
+                case JTokenType.Null:
+                    return VisitValue((JValue)token);
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+
+        protected virtual JToken VisitObject(JObject obj)
+        {
+            foreach (var property in obj.Properties())
+                VisitInternal(property);
+
+            return obj;
+        }
+
+        protected virtual JToken VisitProperty(JProperty property)
+        {
+            VisitInternal(property.Value);
+            return property;
+        }
+
+        protected virtual JToken VisitArray(JArray array)
+        {
+            for (int i = 0; i < array.Count; i++)
+                VisitInternal(array[i]);
+
+            return array;
+        }
+
+        protected virtual JToken VisitValue(JValue value)
+        {
+            return value;
+        }
+    }
+}
